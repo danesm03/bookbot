44435cecb5df3b121c5060ba5de0ff6cc84f80e1
danesm03 <danesmithhh@gmail.com>
2025-07-20 07:02:56 -0400

    character count function works

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from stats import get_wordcount
-    
-def main():
-    book = "books/frankenstein.txt"
-    text = get_book_text(book)
-    print(f"{get_wordcount(text)} words found in the document")
+from stats import get_wordcount, character_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+def main():
+    book = "books/frankenstein.txt"
+    text = get_book_text(book)
+    char_counts = character_counts(text)
+    wc = get_wordcount(text)
+    print(f"{wc} words found in the document")
+    print(char_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@
 def get_wordcount(book):
     words = book.split()
     return len(words)
+
+def character_counts(book):
+    char_count = {} #dict
+    for char in book:
+            lowered = char.lower()
+
+            if lowered in char_count:
+                char_count[lowered] += 1
+            else:
+                char_count[lowered] = 1
+
+    return char_count
+    
